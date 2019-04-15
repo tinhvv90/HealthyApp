d5efd06be5da34486636cbc6698fbcb1f4f8f9b9
Tinhvv <v.ctv_tinhvv@vinid.net>
2019-04-15 23:30:48 +0700

    update logic display Chart money.

--- StepWalk/GoalPopupViewController.swift
+++ StepWalk/GoalPopupViewController.swift
@@ -8,13 +8,41 @@
 
 import UIKit
 
+protocol GoalPopupViewControllerDelegate: class {
+    func getGoalOfDistance(distance: String)
+}
+
 class GoalPopupViewController: UIViewController {
 
     @IBOutlet weak var pickerView: UIPickerView!
+    
+    weak var delegate : GoalPopupViewControllerDelegate?
+    
+    var goalSteps = ["5 Km", "10 Km"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // Do any additional setup after loading the view.
+    }
+    
+    
+}
+
+extension GoalPopupViewController: UIPickerViewDelegate, UIPickerViewDataSource {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return goalSteps.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return goalSteps[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        delegate?.getGoalOfDistance(distance: goalSteps[row])
     }
 }
--- StepWalk/StepsTableViewController.swift
+++ StepWalk/StepsTableViewController.swift
@@ -26,6 +26,12 @@ class StepsTableViewController: UITableViewController {
     var goalStep: Double = 5000
     
     var days:[String] = []
+    var moneyForDistance: [Int] = []
+    var distanceSteps: [Double] = []  {
+        didSet {
+            self.getMoneyForDistance()
+        }
+    }
     var stepsTaken:[Int] = []
     let activityManager = CMMotionActivityManager()
     let pedoMeter = CMPedometer()
@@ -50,6 +56,28 @@ class StepsTableViewController: UITableViewController {
         updateStep()
     }
     
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "goalPopup" {
+            let goalPopupVc = segue.destination as? GoalPopupViewController
+            goalPopupVc?.delegate = self
+            goalPopupVc?.preferredContentSize = CGSize(width: 250, height: 250)
+            goalPopupVc?.popoverPresentationController?.delegate = self
+        }
+    }
+    
+    func getMoneyForDistance() {
+        for step in self.distanceSteps {
+            if Double(step) > 1 && Double(step) < 5000 {
+                self.moneyForDistance.append(10000)
+            } else if Double(step) > 5000 && Double(step) < 8000 {
+                self.moneyForDistance.append(15000)
+            } else if Double(step) > 8000 {
+                self.moneyForDistance.append(20000)
+            }
+        }
+        self.setupProgressView()
+    }
+    
     func updateStep() {
         pedoMeter.startUpdates(from: Date()) { (data, error) in
             self.getDataForLastWeek()
@@ -68,7 +96,7 @@ class StepsTableViewController: UITableViewController {
         weekStepsChartLine.legend.enabled = false
     }
     
-    func updateGraph() {
+    func updateChartDisplaySteps() {
         var lineChartEntry  = [ChartDataEntry]() //this is the Array that will eventually be displayed on the graph.
         
         //here is the for loop
@@ -121,10 +149,64 @@ class StepsTableViewController: UITableViewController {
         self.view.setNeedsDisplay()
     }
     
+    func updateChartDisplayMoney() {
+        var lineChartEntry  = [ChartDataEntry]() //this is the Array that will eventually be displayed on the graph.
+        
+        //here is the for loop
+        for i in 0..<moneyForDistance.count {
+            
+            let value = ChartDataEntry(x: Double(i), y: Double(moneyForDistance[i]), data: days as AnyObject?) // here we set the X and Y status in a data chart entry
+            lineChartEntry.append(value) // here we add it to the data set
+        }
+        
+        let line1 = LineChartDataSet(values: lineChartEntry, label: "") //Here we convert lineChartEntry to a LineChartDataSet
+        
+        //  line1.colors = [NSUIColor.blue] //Sets the colour to blue
+        line1.drawCircleHoleEnabled = true
+        
+        line1.circleHoleColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        line1.setColor(.red)
+        line1.setCircleColor(.red)
+        
+        /// The radius of the drawn circles.
+        line1.circleRadius = 4
+        
+        /// The hole radius of the drawn circles
+        line1.circleHoleRadius = 3
+        
+        line1.lineWidth = 0.3
+        line1.valueFont = .systemFont(ofSize: 9)
+        line1.formLineWidth = 0.3
+        line1.formSize = 15
+        
+        let gradientColors = [ChartColorTemplates.colorFromString("#F01204").cgColor,
+                              ChartColorTemplates.colorFromString("#00ff0000").cgColor
+        ]
+        let gradient = CGGradient(colorsSpace: nil, colors: gradientColors as CFArray, locations: nil)!
+        
+        line1.fillAlpha = 1
+        line1.fill = Fill(linearGradient: gradient, angle: 90) //.linearGradient(gradient, angle: 90)
+        line1.drawFilledEnabled = true
+        
+        let data = LineChartData() //This is the object that will be added to the chart
+        data.addDataSet(line1) //Adds the line to the dataSet
+        
+        
+        pointChartLine.data = data //finally - it adds the chart data to the chart and causes an update
+        
+        let xAxisValue = pointChartLine.xAxis
+        
+        //  xAxisValue.valueFormatter = axisFormatDelegate
+        xAxisValue.valueFormatter = IndexAxisValueFormatter(values: days)
+        
+        self.view.setNeedsDisplay()
+    }
+    
     func getDataForLastWeek() {
         if CMPedometer.isStepCountingAvailable() {
             self.days = []
             self.stepsTaken = []
+            self.distanceSteps = []
             let queue = DispatchQueue(label: "com.example.my-serial-queue")
             
             let formatter = DateFormatter()
@@ -142,6 +224,7 @@ class StepsTableViewController: UITableViewController {
                             print("\(dateStr) : \(data.numberOfSteps)")
                             self.days.append(dateStr)
                             self.stepsTaken.append(Int(data.numberOfSteps))
+                            self.distanceSteps.append(data.distance?.doubleValue ?? 0.0)
                             print("Days :\(self.days)")
                             print("Steps :\(self.stepsTaken)")
                             self.distance = data.distance?.doubleValue ?? 0.0
@@ -159,11 +242,13 @@ class StepsTableViewController: UITableViewController {
     
     func setupProgressView() {
         DispatchQueue.main.async {
-            self.updateGraph()
-            self.stepsCircleView.progress = self.distance / self.goalStep
+            self.updateChartDisplaySteps()
+            self.updateChartDisplayMoney()
             self.stepsNumber.text = "\(Int(self.stepEmtry))"
             let distanceKm = self.distance / 1000
             self.distanceLabel.text = "Da Di: \(self.roundDouble(a: distanceKm)) Km"
+            self.stepsCircleView.progress = self.distance / self.goalStep
+            self.goalLabel.text = "MUC TIEU: \(Int(self.goalStep) / 1000) Km"
             self.tableView.reloadData()
             self.tableView.setNeedsDisplay()
         }
@@ -173,5 +258,39 @@ class StepsTableViewController: UITableViewController {
         let mu = pow(10.0,2.0)
         let r = round( a * mu ) / mu
         return r
+    }
+}
+
+extension StepsTableViewController: UIPopoverPresentationControllerDelegate {
+    func popoverPresentationControllerDidDismissPopover(_ popoverPresentationController: UIPopoverPresentationController) {
+        setAlphaOfBackgroundViews(alpha: 1)
+    }
+    
+    func prepareForPopoverPresentation(_ popoverPresentationController: UIPopoverPresentationController) {
+        setAlphaOfBackgroundViews(alpha: 0.7)
+    }
+    
+    func setAlphaOfBackgroundViews(alpha: CGFloat) {
+        let statusBarWindow = UIApplication.shared.value(forKey: "statusBarWindow") as? UIWindow
+        UIView.animate(withDuration: 0.2) {
+            statusBarWindow?.alpha = alpha;
+            self.view.alpha = alpha;
+            self.navigationController?.navigationBar.alpha = alpha;
+        }
+    }
+    
+    func adaptivePresentationStyle(for controller: UIPresentationController) -> UIModalPresentationStyle {
+        return .none
+    }
+}
+
+extension StepsTableViewController: GoalPopupViewControllerDelegate {
+    func getGoalOfDistance(distance: String) {
+        if distance == "5 Km" {
+            goalStep = 5000
+        } else if distance == "10 Km" {
+            goalStep = 10000
+        }
+        self.setupProgressView()
     }
 }
